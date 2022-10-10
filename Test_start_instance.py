c98b223b5edf8d1de479e95fb4b8716ff24c4fda
Mikulas Vesely <mikface@gmail.com>
2022-10-10 21:30:41 +0200

    delete redundant function

--- Test_start_instance.py
+++ Test_start_instance.py
@@ -1,8 +1,12 @@
 import boto3
+#---------SETTINGS----------------
+imageId = 'ami-08c40ec9ead489470' # ubuntu 22.04
+keyName = 'vockey'
+
+#----------SCRIPT-----------------
+instance_ec2 =  boto3.client("ec2")
 with open('init.sh', 'r') as file:
     initScript = file.read()
-#Creating one single ec2 instance of the type t2.micro
-instance_ec2 =  boto3.client("ec2")
 
 for i in range(9):
     if i < 4:
@@ -11,15 +15,12 @@ for i in range(9):
         instanceType = 'm4.large'
 
     #instanceType = 't2.micro'
-    def create_ec2_instance():
-        print("creating instance, type: " + instanceType + " no." + str(i))
-        instance_ec2.run_instances(
-            ImageId = "ami-08c40ec9ead489470",
-            MinCount = 1,
-            MaxCount = 1,
-            InstanceType = instanceType,
-            KeyName = "vockey",
-            UserData = initScript.replace('$INSTANCE_ID', str(i))
-             )
-
-    create_ec2_instance()
+    print("creating instance, type: " + instanceType + " no." + str(i))
+    instance_ec2.run_instances(
+        ImageId = imageId,
+        MinCount = 1,
+        MaxCount = 1,
+        InstanceType = instanceType,
+        KeyName = keyName,
+        UserData = initScript.replace('$INSTANCE_ID', str(i))
+        )

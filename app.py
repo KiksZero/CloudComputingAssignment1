1432fff8b98ced9d7411e75b0432bc764a4bb389
Mikulas Vesely <mikface@gmail.com>
2022-10-12 03:35:20 +0200

    change url for flask app, read id into variable and close file after

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 f = open("id", "r")
+id = f.read()
+f.close()
 
-@app.route('/')
+@app.route('/cluster1')
 def my_app():
-    return 'Welcome to Instance with id: ' + f.read()
+    return 'Welcome to Instance with id: ' + id
